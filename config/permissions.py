4b5b303ec0f68098764042c09a398a12f792528f
sgaeng <ksge1124@gmail.com>
2023-10-19 04:42:11 +0900

    feat: USER와 COMPANY의 권한을 다르게 주기 위해 permissions of DRF를 사용

--- config/permissions.py
+++ config/permissions.py
@@ -1,21 +1,18 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
-
-# user의 is_company=False 이면 '유저' 이고
-# user의 is_company=True 이면 '회사' 이다.
 
 class IsUser(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and not request.user.is_company
+        return not request.user.is_anonymous and not request.user.is_company
 
 class IsCompany(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.is_company
+        return not request.user.is_anonymous and request.user.is_company
 
 class IsAuthenticatedCompanyOrReadOnlyUser(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_authenticated and request.user.is_company:
-            # 사용자가 회사로 로그인한 경우 수정 권한을 허용
+    def has_permission(self, request, view):
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
-        # 읽기 전용 권한을 모든 다른 사용자에게 부여
-        return request.method in SAFE_METHODS
+        if not request.user.is_anonymous and request.user.is_company:
+            return True
+        return False
